c7d6b9b44e96fe58ca181a611920c954f1d4e0ec
yongxiongzhong <1075321552@qq.com>
2017-06-06 16:19:02 +0800

    Create get_user.h

---
+++ cgi/cgi-bin/get_user.h
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# 定义查询通过id查询用户信息函数
+# 用数组代替数据库查询
+function getUserById {
+    users=(
+        '{"user_id"=>1, "user_name"=>"zhangsan"}'
+        '{"user_id"=>2, "user_name"=>"lisi"}'
+    )
+
+    echo $users[$1]
+}
+
+# 按照CGI协议获取QUERY_STRING里面的请求参数信息
+strArr="$QUERY_STRING"
+id=$(echo $strArr | cut -d = -f 2)
+echo ""
+getUserById $id
